be1b281b8e5fd96153d1e7192fd7acf8fb1baa4b
강경태 <gyeongtae020319@gmail.com>
2020-06-17 19:30:20 +0900

    [UPDATE] Add drag & drop list

--- Practice.v2/B_Module/htdocs/resources/js/store.js
+++ Practice.v2/B_Module/htdocs/resources/js/store.js
@@ -20,6 +20,14 @@ const CHOSEONG = [
     "ㅎ",
 ];
 
+/**숫자에 단위를 맞춰 반점을 넣습니다.
+ */
+Number.prototype.format = function () {
+    return this.valueOf()
+        .toString()
+        .replace(/\d(?=(\d{3})+$)/g, "$&,");
+};
+
 /**문자열의 문자를 초성으로 변환해 반환합니다.
  * - 초성이 없는 문자는 그대로 반환됩니다.
  */
@@ -154,11 +162,11 @@ function updateList(keyword) {
                 for (let i = 0; i < nameSearchData.length; i++) {
                     if (nameSearchData[i].startIndex < endIndex) continue;
                     html += `${name
-                            .text()
-                            .slice(
-                                endIndex,
-                                nameSearchData[i].startIndex
-                            )}<span class="highlight">${
+                        .text()
+                        .slice(
+                            endIndex,
+                            nameSearchData[i].startIndex
+                        )}<span class="highlight">${
                         nameSearchData[i].match
                     }</span>`;
                     endIndex = nameSearchData[i].endIndex;
@@ -170,12 +178,12 @@ function updateList(keyword) {
                 for (let i = 0; i < brandSearchData.length; i++) {
                     if (brandSearchData[i].startIndex < endIndex) continue;
                     html += `${brand
-                            .text()
-                            .slice(
-                                endIndex,
-                                brandSearchData[i].startIndex
-                            )}<span class="highlight">${
-                                brandSearchData[i].match
+                        .text()
+                        .slice(
+                            endIndex,
+                            brandSearchData[i].startIndex
+                        )}<span class="highlight">${
+                        brandSearchData[i].match
                     }</span>`;
                     endIndex = brandSearchData[i].endIndex;
                 }
@@ -190,12 +198,47 @@ function updateList(keyword) {
         });
 }
 
+/**상품의 정보
+ * - 식별 번호
+ * - 사진 이름
+ * - 브랜드
+ * - 이름
+ * - 가격
+ */
+class ProductData {
+    id = 0;
+    photo = "";
+    brand = "";
+    product_name = "";
+    price = "";
+    count_price = 0;
+}
+/**장바구니에 담긴 상품들의 목록
+ * @type {Array<ProductData>}
+ */
+var basketList = [];
+
+function updateTotalPrice() {
+    var total = 0;
+    for (let i = 0; i < basketList.length; i++) {
+        total += basketList[i].count_price;
+    }
+    $("#basket .total-price .value").html(total.format());
+}
+
 $(function () {
     // 상품 목록 불러오기
-    $.getJSON("/resources/json/store.json", function (data) {
-        data.forEach((product, index) => {
-            var item = $(`
-                <div data-index="${index}" class="item">
+    $.getJSON(
+        "/resources/json/store.json",
+        /**상품 목록을 JSON으로부터 가져옵니다.
+         * - 드래그와 드랍 설정
+         *
+         * @param {Array<ProductData>} data
+         */
+        function (data) {
+            data.forEach((product) => {
+                var item = $(`
+                <div data-index="${product.id}" class="item">
                     <div
                         style="
                             background-image: url(/resources/image/product/${product.photo});
@@ -209,24 +252,112 @@ $(function () {
                     </div>
                 </div>
             `);
-            $("#productList .list").append(item);
-        });
+                $("#productList .list").append(item);
+            });
 
-        // 드래그 설정
-        $("#productList .list .item").draggable({
-            containment: "main", // <main> 안에서만 드래그 가능
-            revert: true, // 커서를 놨을 때 제자리로 돌아감
-            revertDuration: 250, // 제자리로 돌아가는데 걸리는 시간 (ms)
-            zIndex: 128, // 드래그할 때 z-index 값
+            // 드래그 설정
+            $("#productList .list .item").draggable({
+                containment: "main", // <main> 안에서만 드래그 가능
+                revert: true, // 커서를 놨을 때 제자리로 돌아감
+                revertDuration: 250, // 제자리로 돌아가는데 걸리는 시간 (ms)
+                zIndex: 128, // 드래그할 때 z-index 값
+            });
+            // 드랍 설정
+            $("#dropArea").droppable({
+                accept: "#productList .list .item", // 드랍 가능한 요소
+                // 드랍 시 이벤트
+                drop: function (event, ui) {
+                    var product =
+                        data[Number(ui.draggable.attr("data-index")) - 1];
+                    product.count_price = Number(
+                        product.price.replace(/,/g, "")
+                    );
+
+                    if (basketList.indexOf(product) > -1) {
+                        alert("이미 장바구니에 담긴 상품입니다.");
+                        return;
+                    }
+                    // 목록 항목 생성
+                    var item = $(`
+                        <div class="item">
+                            <div class="info">
+                                <div
+                                    style="
+                                        background-image: url(/resources/image/product/${product.photo});
+                                    "
+                                    class="background-cover photo"
+                                ></div>
+                                <div class="details">
+                                    <span class="name"
+                                        >${product.product_name}</span
+                                    >
+                                    <span class="brand">${product.brand}</span>
+                                    <span class="price">${product.price}</span>
+                                </div>
+                            </div>
+                            <div class="control">
+                                <div class="label count">
+                                    <span class="name">개수</span>
+                                    <div class="value">
+                                        <input
+                                            type="number"
+                                            value="1"
+                                            min="1"
+                                            name="count"
+                                            class="product-count"
+                                        />
+                                        <button class="button remove"><i class="fa fa-close"></i></button>
+                                    </div>
+                                </div>
+                                <div class="label count-price">
+                                    <span class="name">가격</span>
+                                    <span class="value">${product.price}</span>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+                    // 개수 변경
+                    item.find(".product-count").change((e) => {
+                        var count = e.target.value;
+                        if (count < 1) {
+                            count = e.target.value = 1;
+                        }
+
+                        product.count_price =
+                            Number(product.price.replace(/,/g, "")) * count;
+                        item.find(".count-price .value").html(
+                            product.count_price.format()
+                        );
+
+                        updateTotalPrice();
+                    });
+                    // 항목 제거 버튼
+                    item.find(".remove").click(() => {
+                        basketList.splice(basketList.indexOf(product), 1);
+                        item.remove();
+                    });
+
+                    basketList.push(product);
+                    $("#basket .list").append(item);
+                    updateTotalPrice();
+                },
+            });
+        }
+    );
+
+    // 구매 버튼
+    $("#basket .buy").click(() => {
+        console.log("click");
+        $("#buyInfo").dialog({
+            open: true,
+            buttons: [
+                {
+                    text: "구매",
+                    click: function () {
+                        $(this).dialog("close");
+                    },
+                },
+            ],
         });
-        // 드랍 설정
-        $("#dropArea").droppable({
-            accept: "#productList .list .item", // 드랍 가능한 요소
-            // 드랍 시 이벤트
-            drop: function (event, ui) {
-                var info = data[Number(ui.draggable.attr("data-index"))];
-                console.log(info);
-            },
-        });
-    }).done(function () {});
+    });
 });
